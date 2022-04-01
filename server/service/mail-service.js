65d9e055620f9e587d3df9a874573c9ec28cde1d
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2022-04-01 16:00:45 +0300

    commented nodemailer

--- server/service/mail-service.js
+++ server/service/mail-service.js
@@ -1,31 +1,31 @@
-const nodemailer = require('nodemailer');
-class MailService {
-  constructor() {
-    this.transporter = nodemailer.createTransport({
-      host: process.env.SMPT_HOST,
-      port: process.env.SMPT_PORT,
-      secure: false,
-      auth: {
-        user: process.env.SMPT_USER,
-        pass: process.env.SMPT_PASSWORD,
-      },
-    });
-  }
-  async sendActivationMail(to, link) {
-    console.log('link', link);
-    await this.transporter.sendMail({
-      from: process.env.SMPT_USER,
-      to,
-      subject: 'Активация аккаунта на ' + process.env.API_URL,
-      text: '',
-      html: `
-      <div>
-      <h1>Для активации перейдите по ссылке</h1>
-      <a href=${link}>${link}<a/>
-      </div>
-      `,
-    });
-  }
-}
+// const nodemailer = require('nodemailer');
+// class MailService {
+//   constructor() {
+//     this.transporter = nodemailer.createTransport({
+//       host: process.env.SMPT_HOST,
+//       port: process.env.SMPT_PORT,
+//       secure: false,
+//       auth: {
+//         user: process.env.SMPT_USER,
+//         pass: process.env.SMPT_PASSWORD,
+//       },
+//     });
+//   }
+//   async sendActivationMail(to, link) {
+//     console.log('link', link);
+//     await this.transporter.sendMail({
+//       from: process.env.SMPT_USER,
+//       to,
+//       subject: 'Активация аккаунта на ' + process.env.API_URL,
+//       text: '',
+//       html: `
+//       <div>
+//       <h1>Для активации перейдите по ссылке</h1>
+//       <a href=${link}>${link}<a/>
+//       </div>
+//       `,
+//     });
+//   }
+// }
 
-module.exports = new MailService();
+// module.exports = new MailService();
